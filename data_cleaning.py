cb906da019d700dd071d4e8d63b71e8860b8f7a4
ijaz-hussain <62718643+ijaz-hussain@users.noreply.github.com>
2022-03-22 11:29:50 -0700

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,14 +4,18 @@ import numpy as np
 # Reading original data file
 df = pd.read_csv('no_shows.csv')
 
-# Dropping not needed columns
-df = df.drop(['PatientId'])
-
-#setting all column names to lowercase
+# Setting all column names to lowercase
 df = df.columns.str.lower()
 
+# Dropping not needed columns
+df.drop(['patientid', 'appointmentid', 'scheduledday', 'appointmentday', 'neighbourhood'])
+
+print(df)
+
+#df.rename(index={3: 'hypertension', 6: 'handicap', 8: 'no_show'}, inplace=True)
+
 # Drop ages that are less than 0
-df = df[df.age >= 0]
+#df = df.drop[df.age >= 0]
 
 # Writing cleaned data to new CSV file
-df.to_csv('no_shows_cleaned.csv', index=False)
+#df.to_csv('no_shows_cleaned.csv')
