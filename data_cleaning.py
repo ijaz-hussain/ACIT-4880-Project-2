e9530ddbbe263b793f70ecb75de09d48e9aa758c
ijaz-hussain <62718643+ijaz-hussain@users.noreply.github.com>
2022-03-22 11:44:15 -0700

    update

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,17 +5,26 @@ import numpy as np
 df = pd.read_csv('no_shows.csv')
 
 # Setting all column names to lowercase
-df = df.columns.str.lower()
+df = df.rename(columns=str.lower)
 
 # Dropping not needed columns
-df.drop(['patientid', 'appointmentid', 'scheduledday', 'appointmentday', 'neighbourhood'])
+df = df.drop(columns=['patientid', 'appointmentid', 'scheduledday', 'appointmentday', 'neighbourhood'])
 
-print(df)
-
-#df.rename(index={3: 'hypertension', 6: 'handicap', 8: 'no_show'}, inplace=True)
+# Rename misspelled columns
+df.rename(columns={'hipertension': 'hypertension', 'handcap': 'handicap', 'no-show': 'no_show'}, inplace=True)
 
 # Drop ages that are less than 0
-#df = df.drop[df.age >= 0]
+df = df[df.age >= 0]
+
+# Legal drinking age in most countries is at least 18 - dropping any rows with minors and alcoholism
+#checking
+print(df[(df.alcoholism) & (df.age < 18)].shape)
+
+#dropping them by using their index with the drop method
+df.drop(index= df[(df.alcoholism) & (df.age < 18)].index , inplace=True)
+
+#checking the operation
+print(df[(df.alcoholism) & (df.age < 18)].shape)
 
 # Writing cleaned data to new CSV file
-#df.to_csv('no_shows_cleaned.csv')
+df.to_csv('no_shows_cleaned.csv')
